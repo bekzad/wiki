85e580122f951a144793d5e6eb6c62e600c4e991
Bekzad <n.bekzad@gmail.com>
2020-07-22 07:43:34 +0600

    02.Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -7,5 +8,16 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def wiki(request, title):
+        markdowner = Markdown()
+        if util.get_entry(title):
+            return render(request, "encyclopedia/wiki.html", {
+            "content": markdowner.convert(util.get_entry(title)),
+            "title": title.upper()
+            })
+        else:
+            return render(request, "encyclopedia/error.html")
+    
 
 
